4a5578623b580c731ab1b7dee0b312855b51964f
Ben Boyd <bboyd1997@hotmai.co.uk>
2021-10-27 15:16:59 +0100

    added max depth code to main

--- main.py
+++ main.py
@@ -4,13 +4,15 @@ import numpy as np
 from Evaluation import Evaluation
 
 # Load the dataset
-data_loader = DataLoader("wifi_db/noisy_dataset.txt")
+data_loader = DataLoader("wifi_db/clean_dataset.txt")
 x, y = data_loader.load_data()
 evaluation=Evaluation()
 
 repeat_count = 5
 accuracy1 = np.zeros(repeat_count)
+max_depth1 = np.zeros(repeat_count)
 accuracy2 = np.zeros(repeat_count)
+max_depth2 = np.zeros(repeat_count)
 for i in range(repeat_count):
 
     # Split the dataset
@@ -23,6 +25,7 @@ for i in range(repeat_count):
 
     # Calculate the accuracy of the Decision Tree
     accuracy1[i]=evaluation.evaluate(x_test,y_test,decision_tree)
+    max_depth1[i]=decision_tree.depth
 
 
     # Prune Decision Tree
@@ -30,18 +33,22 @@ for i in range(repeat_count):
 
     # Calculate the accuracy of the pruned Decision Tree
     accuracy2[i]=evaluation.evaluate(x_test,y_test,decision_tree)
+    max_depth2[i]=decision_tree.depth
 
 accuracy1 = sum(accuracy1)/repeat_count * 100
 accuracy1 = "{:.2f}".format(accuracy1)
 print(f"Before Pruning: Average accuracy of {accuracy1}% over {repeat_count} runs")
+print('Average max depth',np.average(max_depth1))
 
 accuracy2 = sum(accuracy2)/repeat_count * 100
 accuracy2 = "{:.2f}".format(accuracy2)
 print(f"After Pruning: Average accuracy of {accuracy2}% over {repeat_count} runs")
+print('Average max depth',np.average(max_depth2))
 
 
 
 decision_tree = DecisionTreeClassifier()
+evaluation=Evaluation()
 
 print('\n \n 10 FOLD CROSS VALIDATION METRICS BEFORE PRUNING:')
 confusion_matrix=evaluation.cross_validation(decision_tree,x,y,10)
