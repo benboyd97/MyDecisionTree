ac596843bd4d318afd3bb12e132f9d6549f9e5a5
Cornelius <cornelius.braun@posteo.de>
2021-10-28 16:50:23 +0100

    confusion matrix now using absolute values that are averaged accross folds

--- Evaluation.py
+++ Evaluation.py
@@ -2,251 +2,132 @@ import numpy as np
 from numpy.random import default_rng
 
 class Evaluation:
-
     def __init__(self):
-        self.matrix=[[0]]
+        self.matrix = [[0]]
 
+    def evaluate(self, x_test, y_test, tree):
+        # predict rooms based on test signals
+        y_preds = tree.predict(x_test)
 
-    def evaluate(self,x_test,y_test,tree):
-
-
-        #predict rooms based on test signals
-        y_preds=tree.predict(x_test)
-
-        #return accuracy
+        # return accuracy
         return np.count_nonzero(y_preds == y_test) / y_test.shape[0]
-
-
 
     def validation_error(self, y_preds, y_test):
         return np.count_nonzero(y_preds != y_test) / y_test.shape[0]
 
-
-
-
-    def cross_validation(self,tree,x,y,k):
-
-        rows,columns=np.shape(x)
-        split_ids=self.k_split(k,rows)
-
-        folds=self.find_folds(k,split_ids)
-
-
-
-        y_actual,y_pred=self.train_test_folds(tree,x,y,k,folds)
-
-
-        
-        self.matrix=self.confusion_matrix(y_actual,y_pred)
-
+    def cross_validation(self, tree, x, y, k):
+        rows, columns = np.shape(x)
+        split_ids = self.k_split(k, rows)
+        folds = self.find_folds(k, split_ids)
+        self.matrix = self.train_test_folds(tree, x, y, folds)
 
         return self.matrix
 
+    # what do we need this for?
+    def pruned_cross_validation(self, tree, x, y, k):
+        rows, columns = np.shape(x)
+        split_ids = self.k_split(k, rows)
+        y_actual_array = np.array([])
+        y_pred_array = np.array([])
 
-
-    def pruned_cross_validation(self,tree,x,y,k):
-
-        rows,columns=np.shape(x)
-
-        split_ids=self.k_split(k,rows)
-
-
-        y_actual_array=np.array([])
-        y_pred_array=np.array([])
         for i in range(k):
+            test_ids = split_ids[i]
+            new_split_ids = split_ids[:i] + split_ids[i + 1:]
+            folds = self.find_folds(k-1, new_split_ids)
+            tree = self.cross_validate_tree(tree, x, y, k-1, folds)
+            y_pred = tree.predict(x[test_ids, :])
+            y_pred_array = np.append(y_pred_array, y_pred)
+            y_actual_array = np.append(y_actual_array, y[test_ids])
 
-            test_ids=split_ids[i]
-
-            new_split_ids=split_ids[:i]+split_ids[i+1:]
-
-
-            folds=self.find_folds(k-1,new_split_ids)
-
-
-            tree=self.cross_validate_tree(tree,x,y,k-1,folds)
-
-            
-            y_pred=tree.predict(x[test_ids,:])
-
-            y_pred_array=np.append(y_pred_array,y_pred)
-
-            y_actual_array=np.append(y_actual_array,y[test_ids])
-
-        
-        self.matrix=self.confusion_matrix(y_actual_array,y_pred_array)
+        self.matrix = self.confusion_matrix(y_actual_array, y_pred_array)
 
         return self.matrix
-            
 
-    def k_split(self,k, rows, random_generator=default_rng()):
-
-
+    def k_split(self, k, rows, random_generator=default_rng()):
         shuffled_ids = random_generator.permutation(rows)
-
         split_ids = np.array_split(shuffled_ids, k)
-
-
         return split_ids
 
-
-    def find_folds(self,k,split_ids):
-
-        folds=[]
-
+    def find_folds(self, k, split_ids):
+        folds = []
         for i in range(k):
-
             test_ids = split_ids[i]
-
-            train_ids = np.hstack(split_ids[:i] + split_ids[i+1:])
-
+            train_ids = np.hstack(split_ids[:i] + split_ids[i + 1:])
             folds.append([train_ids, test_ids])
 
-        
         return folds
 
-
-    def train_test_folds(self,tree,x,y,k,folds):
-
-        y_pred=np.array([])
-        y_actual=np.array([])
-
-
+    def train_test_folds(self, tree, x, y, folds):
+        y_pred = np.array([])
+        y_actual = np.array([])
+        confusion = [[0]]
         for i, (train_ids, test_ids) in enumerate(folds):
             x_train = x[train_ids, :]
             y_train = y[train_ids]
             x_test = x[test_ids, :]
             y_test = y[test_ids]
 
-            
-            tree.fit(x_train,y_train)
+            tree.fit(x_train, y_train)
+            y_pred = np.append(y_pred, tree.predict(x_test))
+            y_actual = np.append(y_actual, y_test)
+            confusion += self.confusion_matrix(y_actual, y_pred)
 
-            y_pred=np.append(y_pred,tree.predict(x_test))
+        return confusion / len(folds)
 
-            y_actual=np.append(y_actual,y_test)
-
-
-        return y_actual,y_pred
-
-
-    def cross_validate_tree(self,tree,x,y,k,folds):
-        
-        
-        val_accuracy=np.zeros(k)
-        
+    def cross_validate_tree(self, tree, x, y, k, folds):
+        val_accuracy = np.zeros(k)
         for j, (train_ids, val_ids) in enumerate(folds):
-
             x_train = x[train_ids, :]
             y_train = y[train_ids]
             x_val = x[val_ids, :]
             y_val = y[val_ids]
 
-            tree.fit(x_train,y_train)
-
+            tree.fit(x_train, y_train)
             tree.prune(x_train, y_train, x_val, y_val)
+            val_accuracy[j] = self.evaluate(x_val, y_val, tree)
 
-            val_accuracy[j]=self.evaluate(x_val,y_val,tree)
+        # get the best model params
+        best_val_id = np.argmax(val_accuracy)
+        best_x_train = x[folds[best_val_id][0], :]
+        best_y_train = y[folds[best_val_id][0]]
+        best_x_val = x[folds[best_val_id][1], :]
+        best_y_val = y[folds[best_val_id][1]]
 
-        
-        
-        best_val_id=np.argmax(val_accuracy)
-
-        best_x_train=x[folds[best_val_id][0],:]
-        best_y_train=y[folds[best_val_id][0]]
-
-        best_x_val=x[folds[best_val_id][1],:]
-        best_y_val=y[folds[best_val_id][1]]
-
-        tree.fit(best_x_train,best_y_train)
-
-        tree.prune(best_x_train,best_y_train,best_x_val,best_y_val)
+        # return the trained and pruned model
+        tree.fit(best_x_train, best_y_train)
+        tree.prune(best_x_train, best_y_train, best_x_val, best_y_val)
 
         return tree
-        
 
-
-
-    def confusion_matrix(self,y_actual, y_pred, class_labels=[1,2,3,4]):
-
-
+    def confusion_matrix(self, y_actual, y_pred, class_labels=None):
+        if class_labels is None:
+            class_labels = np.unique(np.concatenate((y_pred, y_actual)))
 
         confusion = np.zeros((len(class_labels), len(class_labels)), dtype=np.int)
 
-        for (i, label) in enumerate(class_labels):
+        for label in class_labels:
+            if label in y_actual:
+                for pred in y_pred[y_actual == label]:
+                    confusion[int(label)-1, int(pred)-1] += 1
 
-            indices = (y_actual == label)
-            pred = y_pred[indices]
+        return confusion
 
-            (unique_labels, counts) = np.unique(pred, return_counts=True)
-
-            frequency_dict = dict(zip(unique_labels, counts))
-
-            for (j, class_label) in enumerate(class_labels):
-                confusion[i, j] = frequency_dict.get(class_label, 0)
-
-        
-
-        return confusion/len(y_actual)
-
-    
     def accuracy_from_confusion(self):
-        confusion=self.matrix
+        confusion = self.matrix
         if np.sum(confusion) > 0:
-
-            return np.sum(np.diag(confusion)) / np.sum(confusion)
+            return np.trace(confusion) / np.sum(confusion)
         else:
             return 0.
 
-
-
-    def TPTN_FPFN(self,class_val):
-        matrix=self.matrix
-        
-        TP=matrix[class_val,class_val]
-        TN=np.sum(np.diag(matrix)) - TP
-
-        FP=sum(matrix[:,class_val])-TP
-        FN=sum(matrix[class_val,:])-TP
-
-    
-        return TP,TN,FP,FN
-
-    
     def recall(self):
-        
-        rows,columns=np.shape(self.matrix)
-
-        recall_array=np.zeros(rows)
-
-        for i in range(rows):
-
-            TP,TN,FP,FN=self.TPTN_FPFN(i)
-
-
-            recall_array[i]= TP/(TP+FN)
-
-
-        return recall_array
-
+        confusion = self.matrix
+        return np.diagonal(confusion) / np.sum(confusion, axis=1)
 
     def precision(self):
-        
-        rows,columns=np.shape(self.matrix)
-
-        precision_array=np.zeros(rows)
-
-        for i in range(rows):
-
-            TP,TN,FP,FN=self.TPTN_FPFN(i)
-
-
-            precision_array[i]= TP/(TP+FP)
-
-        return precision_array
-
+        confusion = self.matrix
+        return np.diagonal(confusion) / np.sum(confusion, axis=0)
 
     def F1(self):
-
-        precision=self.precision()
-        recall=self.recall()
-
-        return (2*precision*recall)/(precision+recall)
+        precision = self.precision()
+        recall = self.recall()
+        return (2 * precision * recall) / (precision + recall)
--- main.py
+++ main.py
@@ -2,11 +2,18 @@ from DecisionTreeClassifier import DecisionTreeClassifier
 from DataLoader import DataLoader
 import numpy as np
 from Evaluation import Evaluation
+import argparse
+
+parser = argparse.ArgumentParser(description="Decision tree classifier for the Introduction to Machine Learning Coursework 1")
+parser.add_argument('--data', type=str, help="Data to classify (e.g. 'clean', 'noisy') ", default="noisy")
+args = parser.parse_args()
+
+print(f"Working on {args.data} data")
 
 # Load the dataset
-data_loader = DataLoader("wifi_db/noisy_dataset.txt")
+data_loader = DataLoader("wifi_db/" + args.data + "_dataset.txt") # TODO: we have to be careful here, as far as I know the forward slash does not work on Windows?
 x, y = data_loader.load_data()
-evaluation=Evaluation()
+evaluation = Evaluation()
 
 repeat_count = 5
 accuracy1 = np.zeros(repeat_count)
@@ -14,7 +21,6 @@ max_depth1 = np.zeros(repeat_count)
 accuracy2 = np.zeros(repeat_count)
 max_depth2 = np.zeros(repeat_count)
 for i in range(repeat_count):
-
     # Split the dataset
     x_train, x_test, x_val, y_train, y_test, y_val = data_loader.split_dataset(x, y, 0.1, 0.1)
 
@@ -22,51 +28,49 @@ for i in range(repeat_count):
     decision_tree = DecisionTreeClassifier()
     decision_tree.fit(x_train, y_train)
 
-
     # Calculate the accuracy of the Decision Tree
-    accuracy1[i]=evaluation.evaluate(x_test,y_test,decision_tree)
-    max_depth1[i]=decision_tree.depth
-
+    accuracy1[i] = evaluation.evaluate(x_test, y_test, decision_tree)
+    max_depth1[i] = decision_tree.depth
 
     # Prune Decision Tree
     decision_tree.prune(x_train, y_train, x_val, y_val)
 
     # Calculate the accuracy of the pruned Decision Tree
-    accuracy2[i]=evaluation.evaluate(x_test,y_test,decision_tree)
-    max_depth2[i]=decision_tree.depth
+    accuracy2[i] = evaluation.evaluate(x_test, y_test, decision_tree)
+    max_depth2[i] = decision_tree.depth
 
-accuracy1 = sum(accuracy1)/repeat_count * 100
+accuracy1 = sum(accuracy1) / repeat_count * 100
 accuracy1 = "{:.2f}".format(accuracy1)
 print(f"Before Pruning: Average accuracy of {accuracy1}% over {repeat_count} runs")
-print('Average max depth',np.average(max_depth1))
+print('Average max depth', np.average(max_depth1))
 
-accuracy2 = sum(accuracy2)/repeat_count * 100
+accuracy2 = sum(accuracy2) / repeat_count * 100
 accuracy2 = "{:.2f}".format(accuracy2)
 print(f"After Pruning: Average accuracy of {accuracy2}% over {repeat_count} runs")
-print('Average max depth',np.average(max_depth2))
-
-
+print('Average max depth', np.average(max_depth2))
 
 decision_tree = DecisionTreeClassifier()
-evaluation=Evaluation()
+evaluation = Evaluation()
+
+# make sure we just print two decimals
+np.set_printoptions(precision=2)
 
 print('\n \n 10 FOLD CROSS VALIDATION METRICS BEFORE PRUNING:')
-confusion_matrix=evaluation.cross_validation(decision_tree,x,y,10)
+confusion_matrix = evaluation.cross_validation(decision_tree, x, y, 10)
 print('Confusion Matrix:')
 print(confusion_matrix)
-print('Accuracy: ', evaluation.accuracy_from_confusion())
-print('Precision: ',evaluation.precision())
-print('Recall: ',evaluation.recall())
-print('F1: ',evaluation.F1())
+print(f'Accuracy: {evaluation.accuracy_from_confusion():.2f}')
+print('Precision: ', evaluation.precision())
+print('Recall: ', evaluation.recall())
+print('F1: ', evaluation.F1())
 
 decision_tree = DecisionTreeClassifier()
 
 print('\n\n 10 FOLD CROSS VALIDATION METRICS AFTER PRUNING:')
-confusion_matrix=evaluation.pruned_cross_validation(decision_tree,x,y,10)
+confusion_matrix = evaluation.pruned_cross_validation(decision_tree, x, y, 10)
 print('Confusion Matrix:')
 print(confusion_matrix)
-print('Accuracy: ', evaluation.accuracy_from_confusion())
-print('Precision: ',evaluation.precision())
-print('Recall: ',evaluation.recall())
-print('F1: ',evaluation.F1())
-
+print(f'Accuracy: {evaluation.accuracy_from_confusion():.2f}')
+print('Precision: ', evaluation.precision())
+print('Recall: ', evaluation.recall())
+print('F1: ', evaluation.F1())
