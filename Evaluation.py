4e2aa6d4c5a8ae30bcec662b4a2fcded9a7f257e
Ben Boyd <bboyd1997@hotmai.co.uk>
2021-11-01 17:36:59 +0000

    Now average results of inner trees and added max depth into cross-validation

--- Evaluation.py
+++ Evaluation.py
@@ -16,32 +16,54 @@ class Evaluation:
     def validation_error(self, y_preds, y_test):
         return np.count_nonzero(y_preds != y_test) / y_test.shape[0]
 
-        #regular cross validation used for tests with no validation set
-    def unnested_cross_validation(self, tree, x, y, k):
+        #trains the model on k different folds and finds average confusion matrix
+    def cross_validation(self, tree, x, y, k):
         rows, columns = np.shape(x)
         split_ids = self.k_split(k, rows)
         folds = self.find_folds(k, split_ids)
-        self.matrix = self.cross_validation(tree, x, y, folds)
-
+        confusion = [[0]]
+        depth=0
+        for i, (train_ids, test_ids) in enumerate(folds):
+            x_train = x[train_ids, :]
+            y_train = y[train_ids]
+            x_test = x[test_ids, :]
+            y_test = y[test_ids]
+            tree.fit(x_train, y_train)
+            y_pred = tree.predict(x_test)
+            depth+=tree.calc_depth(tree.root)
+            confusion += self.confusion_matrix(y_test,y_pred)
+        self.average_depth=depth/k
+        self.matrix=confusion/k
         return self.matrix
 
-    #contains the outer loop for nested cross-validation by averaging the 10 best confusion matrices
+    #option 2 nested cross validation finds the average confusion matrix based on k outer folds and k-1 inner folds
     def nested_cross_validation(self, tree, x, y, k):
         rows, columns = np.shape(x)
         split_ids = self.k_split(k, rows)
-        confusion = [[0]]
-
+        outer_confusion = [[0]]
+        outer_depth=0
         for i in range(k):
+            inner_confusion = [[0]]
+            inner_depth=0
             test_ids = split_ids[i]
+            x_test= x[test_ids, :]
+            y_test = y[test_ids]
             new_split_ids = split_ids[:i] + split_ids[i + 1:]
             folds = self.find_folds(k-1, new_split_ids)
-            tree = self.cross_validation_hyperparam_tuning(tree, x, y, k-1, folds)
-            y_pred = tree.predict(x[test_ids, :])
-            y_test = y[test_ids]
-            confusion += self.confusion_matrix(y_test, y_pred)
-
-        self.matrix = confusion / k
-
+            for j, (train_ids, val_ids) in enumerate(folds):
+                x_train = x[train_ids, :]
+                y_train = y[train_ids]
+                x_val = x[val_ids, :]
+                y_val = y[val_ids]
+                tree.fit(x_train, y_train)
+                tree.prune(x_train, y_train, x_val, y_val)
+                y_pred = tree.predict(x_test)
+                inner_depth+=tree.calc_depth(tree.root)
+                inner_confusion += self.confusion_matrix(y_test, y_pred)
+            outer_confusion+= inner_confusion/(k-1)
+            outer_depth+=inner_depth/(k-1)
+        self.average_depth=outer_depth/k
+        self.matrix=outer_confusion/k
         return self.matrix
 
     #split data ids into k evenly sized chunks 
@@ -60,48 +82,6 @@ class Evaluation:
 
         return folds
 
-
-    #trains the model on k different folds and finds average confusion matrix
-    def cross_validation(self, tree, x, y, folds):
-        confusion = [[0]]
-        for i, (train_ids, test_ids) in enumerate(folds):
-            x_train = x[train_ids, :]
-            y_train = y[train_ids]
-            x_test = x[test_ids, :]
-            y_test = y[test_ids]
-            tree.fit(x_train, y_train)
-            y_pred = tree.predict(x_test)
-            confusion += self.confusion_matrix(y_test,y_pred)
-
-        return confusion / len(folds)
-
-    def cross_validation_hyperparam_tuning(self, tree, x, y, k, folds):
-        """
-        inner loop of nested cross validation used to find k-1 best models
-        """
-        val_accuracy = np.zeros(k)
-        for j, (train_ids, val_ids) in enumerate(folds):
-            x_train = x[train_ids, :]
-            y_train = y[train_ids]
-            x_val = x[val_ids, :]
-            y_val = y[val_ids]
-
-            tree.fit(x_train, y_train)
-            tree.prune(x_train, y_train, x_val, y_val)
-            val_accuracy[j] = self.evaluate(x_val, y_val, tree)
-
-        # get the best model params
-        best_val_id = np.argmax(val_accuracy)
-        best_x_train = x[folds[best_val_id][0], :]
-        best_y_train = y[folds[best_val_id][0]]
-        best_x_val = x[folds[best_val_id][1], :]
-        best_y_val = y[folds[best_val_id][1]]
-
-        # return the trained and pruned model
-        tree.fit(best_x_train, best_y_train)
-        tree.prune(best_x_train, best_y_train, best_x_val, best_y_val)
-
-        return tree
 
     #finds the confusion matrix
     def confusion_matrix(self, y_actual, y_pred, class_labels=None):
@@ -141,3 +121,8 @@ class Evaluation:
         precision = self.precision()
         recall = self.recall()
         return (2 * precision * recall) / (precision + recall)
+
+
+    #outputs average depth
+    def average_max_depth(self):
+        return self.average_depth
